5f6bf89ede2d13c1220dc4ea757ecb40c31ef427
Gautami2 <118469303+Gautami2@users.noreply.github.com>
2023-11-24 17:20:25 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ const express = require("express");
 const app = express();
 require("dotenv").config()
 
-app.get('/8000/',(req,res) => {
+app.get('/api/get',(req,res) => {
     res.send({message:"hello back to nodejs"})
 })
 
 app.get('/api/get_user_details',(req,res) => {
     res.send({
         user:{
-            name:"satyam",
+            name:"gautami",
             age:22,
             contact:12254
         },env:process.env.NAME
